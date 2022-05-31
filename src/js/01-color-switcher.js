af55daea4b7a6e51516db0a4564512cd8bfe6aab
dawidmarek95x <dawid.marek95x@gmail.com>
2022-06-01 00:06:44 +0200

    Task 1 completed
    Added script that causes the background color of the body element to change to a random value once per second when the Start button is clicked. however, when you click the Stop button, the background color stops changing.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
 
+// Search for the start button
+const startBtn = document.querySelector("button[data-start]");
+
+// Search for the stop button
+const stopBtn = document.querySelector("button[data-stop]");
+
+// Locking the stop button after entering the website
+lockingBtn(stopBtn);
+
+// Declaration of a timer variable
+let changingBgColorTimer = null;
+
+// Listener for "click" events for the start button
+startBtn.addEventListener("click", changingBodyBgColor);
+
+// Function to change the background color of the body element
+function changingBodyBgColor() {
+  const styleForBody =  document.body.style;
+  styleForBody.backgroundColor = getRandomHexColor();
+  lockingBtn(startBtn);
+  unlockingBtn(stopBtn);
+  changingBgColorTimer = setInterval(() => {
+    styleForBody.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+// Listener for "click" events for the stop button
+stopBtn.addEventListener("click", stopChangingBgColor);
+
+// Function that stops changing the background color
+function stopChangingBgColor() {
+  clearInterval(changingBgColorTimer);
+  lockingBtn(stopBtn);
+  unlockingBtn(startBtn);
+}
+
+// Function that locks the selected button
+function lockingBtn(btnName) {
+  btnName.disabled = true;
+}
+
+// Function that unlocks the selected button
+function unlockingBtn(btnName) {
+  btnName.disabled = false;
+}
+
+// A function that generates a random color in HEX format
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
